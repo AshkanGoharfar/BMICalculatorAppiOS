0f4080e5e738d9c5cb88202bb6bf05c415f05420
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-12-16 12:13:22 +0300

    New Sfwidts and View Controllers for Table View and Edit screen have been created.

--- BMICalculatorAppiOS/ViewController.swift
+++ BMICalculatorAppiOS/ViewController.swift
@@ -11,7 +11,26 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Personal Information Screen"
+        view.backgroundColor = .systemGray6
+        
+        let buttonDone = UIButton(frame: CGRect(x: 30, y:720, width: 130, height: 50))
+        view.addSubview(buttonDone)
+//        buttonDone.center = view.center
+        buttonDone.backgroundColor = .systemMint
+        buttonDone.setTitle("Done", for: .normal)
+        buttonDone.addTarget(self, action: #selector(didTapDoneButton), for: .touchUpInside)
+        
+        configureItems()
+    }
+    
+    private func configureItems() {
+    }
+    
+    @objc func didTapDoneButton () {
+        let vc = UIViewController()
+        vc.title = "BMI Tracking Screen"
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 
