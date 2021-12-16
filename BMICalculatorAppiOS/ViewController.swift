6a7251d0545fe67a0dc9a069e420453993932e64
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-12-16 12:49:10 +0300

    First screen has been implemented.

--- BMICalculatorAppiOS/ViewController.swift
+++ BMICalculatorAppiOS/ViewController.swift
@@ -5,32 +5,71 @@
 //  Created by Ashkan Goharfar on 9/25/1400 AP.
 //
 
+import RealmSwift
 import UIKit
+
+/**
+ * Create To do list object for database and other purposes as it includes a tasks parameters like name of the task, description of the task and etc.
+ */
+class BMIListObject: Object {
+    @objc dynamic var name: String = ""
+    @objc dynamic var age: String = ""
+    @objc dynamic var id: String = ""
+    @objc dynamic var gender: String = ""
+    @objc dynamic var scroingType: Bool = false
+    @objc dynamic var weight: String = ""
+    @objc dynamic var height: String = ""
+}
+
 
 class ViewController: UIViewController {
 
+    private let realmDB = try! Realm()
+    private var data = [BMIListObject]()
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var bMITypeLabel: UILabel!
+    @IBOutlet weak var weightTypeLabel: UILabel!
+    @IBOutlet weak var heightTypeLabel: UILabel!
+    
+    @IBOutlet weak var currentBMIValue: UILabel!
+    
+    @IBOutlet weak var bMIMessageString: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Personal Information Screen"
         view.backgroundColor = .systemGray6
         
-        let buttonDone = UIButton(frame: CGRect(x: 30, y:720, width: 130, height: 50))
+        let buttonDone = UIButton(frame: CGRect(x: 30, y:760, width: 130, height: 50))
         view.addSubview(buttonDone)
 //        buttonDone.center = view.center
         buttonDone.backgroundColor = .systemMint
         buttonDone.setTitle("Done", for: .normal)
         buttonDone.addTarget(self, action: #selector(didTapDoneButton), for: .touchUpInside)
-        
-        configureItems()
     }
     
-    private func configureItems() {
-    }
+    
     
     @objc func didTapDoneButton () {
-        let vc = UIViewController()
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "BMITable") as? BMITableViewController else {
+            return
+        }
         vc.title = "BMI Tracking Screen"
+        vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // This function will give all of data from Realm database and will reload our tableview
+    func refresh()
+    {
+        // Update the data according to the database
+        data = realmDB.objects(BMIListObject.self).map({ $0 })
     }
 
 
