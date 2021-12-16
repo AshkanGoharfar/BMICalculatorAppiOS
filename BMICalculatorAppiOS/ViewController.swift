4d5419bdece193da910a32110723580b1f759419
Ashkan Goharfar <ashkan.goharfar@gmail.com>
2021-12-16 10:40:34 +0300

    Initial Commit

---
+++ BMICalculatorAppiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICalculatorAppiOS
+//
+//  Created by Ashkan Goharfar on 9/25/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
